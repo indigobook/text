64b3b8d00e6bca77ff523a4b69b2f0e113a7a209
Frank Bennett <biercenator@gmail.com>
2021-07-17 23:46:41 +0900

    Fix jurisdiction field in extracted data, expanding to full parseable form

--- ib-convert.js
+++ ib-convert.js
@@ -1,8 +1,21 @@
 const fs = require("fs");
+const path = require("path");
 const dom = require("htmldom2").DOMParser;
 const serializer = require("htmldom2").XMLSerializer;
 const xpath = require("xpath");
 const pretty = require('pretty');
+
+/*
+ * Paths
+ */
+const jurisMapPath = (fn) => {
+    var stub = path.join("..", "JM", "jurism", "juris-maps");
+    if (fn) {
+        return path.join(stub, fn);
+    } else {
+        return stub;
+    }
+}
 
 /*
   Only these node types are of interest
@@ -16,7 +29,8 @@ const processTypes = {
 /*
   Initialize original DOM as source
 */
-var html_txt = fs.readFileSync("sample-saved-as-web-page.html").toString();
+// Saved as Web Page from Word
+var html_txt = fs.readFileSync("sample.html").toString();
 var doc = new dom().parseFromString(html_txt, "text/html");
 var body = xpath.select("//body", doc)[0];
 
@@ -38,6 +52,56 @@ var newbody = xpath.select("//body", newdoc)[0];
   Nodes are appended to the node at the end of the "stack" array.
 */
 var stack = [newbody];
+
+const padding = (num) => {
+    num = num.toString();
+    while (num.length < 3) {
+        num = `0${num}`;
+    }
+    return num;
+}
+
+const loadJurisdictionMaps = (code) => {
+    var accByPos = {};
+    var accByKey = {};
+    // Fetch map file
+    var _jurisdictionKeySplit = code.split(":");
+    var topJurisdiction = _jurisdictionKeySplit[0];
+    console.log(`Loading jurisdiction codes for: ${topJurisdiction}`);
+    
+    var jurisdictionFile = `juris-${topJurisdiction}-map.json`;
+    var jurisdictionJSON = fs.readFileSync(jurisMapPath(jurisdictionFile)).toString();
+    var jurisdictionRawData = JSON.parse(jurisdictionJSON);
+    // console.log(JSON.stringify(jurisdictionRawData, null, 2));
+
+    var jurisdictionList = jurisdictionRawData.jurisdictions.default;
+    accByPos[0] = {
+        offset: padding(jurisdictionList[0][0].length),
+        code: jurisdictionList[0][0],
+        name: `${jurisdictionList[0][1]}|${jurisdictionList[0][0].toUpperCase()}`
+    }
+
+    for (var i=1,ilen=jurisdictionList.length; i<ilen; i++) {
+        var jurisdictionInfo = jurisdictionList[i];
+        var parentPos = jurisdictionInfo[2];
+        //console.log(JSON.stringify(jurisdictionInfo, null, 2));
+        var parentInfo = accByPos[parentPos];
+        var code = `${parentInfo.code}:${jurisdictionInfo[0]}`;
+        var name = `${parentInfo.name}|${jurisdictionInfo[1]}`;
+        accByPos[i] = {
+            offset: padding(code.length),
+            code: code,
+            name: name
+        }
+    }
+    for (var pos in accByPos) {
+        var info = accByPos[pos];
+        accByKey[info.code] = `${info.offset}${info.code}${info.name}`;
+    }
+    // console.log(JSON.stringify(accByKey, null, 2));
+    // process.exit();
+    return accByKey;
+}
 
 /*
   Set the output node, or return null to ignore
@@ -88,7 +152,7 @@ const checkNode = (node) => {
 /*
   Recursive scraper function
 */
-const checkEachNode = (parent, node, depth) => {
+const checkEachNode = (jurisdictionMap, parent, node, depth) => {
     var type = processTypes[node.nodeType];
     if (type === "text") {
 	    var content = node.nodeValue.replace(/&nbsp;/g, " ").replace(/¥s+/g, " ");
@@ -114,7 +178,7 @@ const checkEachNode = (parent, node, depth) => {
 	        }
 	        for(var i=0; i<node.childNodes.length; i++) {
 		        //console.log(`PUSHED parent ${newnode.tagName} depth is ${depth}, stack length is: ${stack.length}`);
-		        checkEachNode(newparent, node.childNodes[i], depth+1);
+		        checkEachNode(jurisdictionMap, newparent, node.childNodes[i], depth+1);
 	        }
 	    }
     } else if (type === "comment") {
@@ -128,6 +192,11 @@ const checkEachNode = (parent, node, depth) => {
             if (begin.length) {
                 var fieldJSON = fieldBody.firstChild.textContent.slice(32).replace(/&quot;/g, '"').replace(/(\r\n|\n|\r)/gm, " ").replace(/formattedCitation.*plainCitation/g, "plainCitation").trim();
                 var fieldObj = JSON.parse(fieldJSON);
+                for (var itemObj of fieldObj.citationItems) {
+                    if (itemObj.itemData.jurisdiction) {
+                        itemObj.itemData.jurisdiction = jurisdictionMap[itemObj.itemData.jurisdiction];
+                    }
+                }
                 console.log(`HOORAY, HERE IS THE JSON DATA FOR ONE CITATION:`);
                 console.log(`${JSON.stringify(fieldObj, null, 2)}`);
             }
@@ -135,7 +204,8 @@ const checkEachNode = (parent, node, depth) => {
     }
 }
 
-checkEachNode(newbody, body,0);
+var jurisdictionMap = loadJurisdictionMaps("us");
+checkEachNode(jurisdictionMap, newbody, body,0);
 
 console.log("");
 console.log("HOORAY AGAIN, HERE IS SOME HTML");
